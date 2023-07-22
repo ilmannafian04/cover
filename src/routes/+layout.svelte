f060f9c50a56c7fc99f663dc33f24c8cda4fd44e
ilmannafian04 <milmannafian04@gmail.com>
2023-07-22 16:07:09 +0700

    feat: basic intro

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import '../app.css';
+</script>
+
+<div class="h-screen w-screen flex items-center justify-center text-white">
+  <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-row items-center">
+  <span class="text-3xl">Hi, i'm Ilman</span>
+</div>
